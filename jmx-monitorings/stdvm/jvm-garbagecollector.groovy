bbf0245962a6d128f2b64b9ce934e6a860c9f5b3
killbulle <marco.godin@gmail.com>
2011-05-13 13:46:43 +0200

    fix alias

--- jmx-monitorings/stdvm/jvm-garbagecollector.groovy
+++ jmx-monitorings/stdvm/jvm-garbagecollector.groovy
@@ -1,8 +1,8 @@
 collector.patternMonitoring(pattern: "java.lang:type=GarbageCollector", namekey: "name") {
-    metric(name: "CollectionCount", alias: "nb")
-    metric(name: "CollectionTime",alias:"tms")
-    metric(name: "LastGcInfo.duration", alias: "lstgctime")
-    metric(name: "LastGcInfo.memoryUsageAfterGc.PS Eden Space.used", alias: "EdAft")
-    metric(name: "LastGcInfo.memoryUsageAfterGc.PS Old Gen.used", alias: "OldAft")
-    metric(name: "LastGcInfo.memoryUsageAfterGc.PS Perm Gen.used",alias: "PermAft")
+    metric(name: "CollectionCount")
+    metric(name: "CollectionTime")
+    metric(name: "LastGcInfo.duration")
+    metric(name: "LastGcInfo.memoryUsageAfterGc.PS Eden Space.used")
+    metric(name: "LastGcInfo.memoryUsageAfterGc.PS Old Gen.used")
+    metric(name: "LastGcInfo.memoryUsageAfterGc.PS Perm Gen.used")
 }
